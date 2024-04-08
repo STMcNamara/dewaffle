6afdb2b72210a182ad10b075b29b29ea0639dfd5
Sean McNamara <stmcnamara26@gmail.com>
2024-04-08 21:17:30 +0100

    Added basic placeholder endpoint.

--- dewaffle-api/dewaffle_api/main.py
+++ dewaffle-api/dewaffle_api/main.py
@@ -1,8 +1,27 @@
 from fastapi import FastAPI
 
+from dewaffle_api.schema import Recipe, RecipeURL, Ingredient, UnitsEnum
+
 app = FastAPI()
 
 
 @app.get("/")
 async def root():
-    return{"message": "Hello world - welcome to Dewaffle"}
+    return {"message": "Hello world - welcome to Dewaffle"}
+
+
+@app.post("/recipe-from-url")
+async def from_url(
+        url: RecipeURL
+) -> Recipe:
+    place_holder_recipe: Recipe = Recipe(
+        name='placeholder_name',
+        origin_url=url,
+        ingredients= [Ingredient(
+            name="Flour",
+            quantity="2",
+            units=UnitsEnum.GRAMS
+        )],
+        steps=["1. Start", "N. Finish"]
+    )
+    return place_holder_recipe
---
+++ dewaffle-api/dewaffle_api/schema.py
@@ -0,0 +1,27 @@
+from enum import StrEnum
+from typing import List, Optional
+
+from pydantic import BaseModel, AnyHttpUrl
+
+
+class UnitsEnum(StrEnum):
+    GRAMS = "g"
+    KILOGRAMS = "kg"
+    MILLILITRES = "ml"
+
+
+class Ingredient(BaseModel):
+    name: str
+    quantity: float
+    units: UnitsEnum
+
+
+class RecipeURL(BaseModel):
+    url: AnyHttpUrl
+
+
+class Recipe(BaseModel):
+    name: str
+    origin_url: Optional[RecipeURL]
+    ingredients: List[Ingredient]
+    steps: List[str]
