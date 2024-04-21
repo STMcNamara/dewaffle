b16155094965d41373a385732b18633d48395bea
Sean McNamara <stmcnamara26@gmail.com>
2024-04-21 17:20:25 +0100

    Updated linting config and resolved formatting errors.

--- dewaffle-api/dewaffle_api/main.py
+++ dewaffle-api/dewaffle_api/main.py
@@ -16,10 +16,9 @@ async def from_url(url: RecipeURL) -> Recipe:
     place_holder_recipe: Recipe = Recipe(
         name="placeholder_name",
         origin_url=url,
-        ingredients=[Ingredient(
-            name="Flour",
-            quantity="2",
-            units=UnitsEnum.GRAMS)],
+        ingredients=[
+            Ingredient(name="Flour", quantity="2", units=UnitsEnum.GRAMS)
+        ],
         steps=["1. Start", "N. Finish"],
     )
     return place_holder_recipe
